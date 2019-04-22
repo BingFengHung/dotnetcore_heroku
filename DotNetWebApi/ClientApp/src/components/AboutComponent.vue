fcd08382d2748d7369486460fe6574cfe47bd687
joe <joe94008@gmail.com>
2019-04-22 20:12:54 +0800

    feat: 嘗試使用同 server 下的 api
    嘗試使用同 server 下的 api
    http://localhost:3000/api/values

---
+++ DotNetWebApi/ClientApp/src/components/AboutComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <h2>Hello</h2>
+      <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class AboutComponent extends Vue {
+  @Prop() private msg!: string;
+  mounted() {
+      alert('in');
+      Vue.axios.get("http://localhost:3000/api/values").then((response) => {
+          alert(JSON.stringify(response));
+      });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- DotNetWebApi/ClientApp/src/views/About.vue
+++ DotNetWebApi/ClientApp/src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <AboutComponent msg='OhYa'/>
   </div>
+  
 </template>
+
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+import AboutComponent from '@/components/AboutComponent.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    AboutComponent
+  }
+})
+
+export default class About extends Vue {}
+</script>
